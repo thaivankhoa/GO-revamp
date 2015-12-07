07350cd79159c16550af671f869ede5d0b9e90e7
maggie1994 <tuonglinhtong@gmail.com>
2015-12-07 22:12:44 +0700

    Update feature/work-section version 1

--- app/assets/javascripts/myjs.js
+++ app/assets/javascripts/myjs.js
@@ -2,8 +2,9 @@ $('document').ready(function() {
   	var mn = $('.menu-section');
   	var mns = 'menu-section-scroll';
   	var hdr = $('.header-section').height();
+    var service = $('.header-section').height()+$('.menu-section').height()+$('.service-section').height();
   	$(window).scroll(function(){
-  		if($(this).scrollTop() > hdr){
+  		if($(this).scrollTop() > service){
   			mn.addClass(mns);
   		}else{
   			mn.removeClass(mns);
