d4de156cb61ab0bc29860e0974713be2426d7ee8
maggie1994 <tuonglinhtong@gmail.com>
2015-12-08 09:41:53 +0700

    Update service-section version 4

--- app/assets/javascripts/myjs.js
+++ app/assets/javascripts/myjs.js
@@ -1,13 +1,18 @@
 $('document').ready(function() {
   	var mn = $('.menu-section');
-  	var mns = 'menu-section-scroll';
+  	var mns_01 = 'menu-section-scroll-01';
   	var hdr = $('.header-section').height();
     var service = $('.header-section').height()+$('.menu-section').height()+$('.service-section').height();
   	$(window).scroll(function(){
-  		if($(this).scrollTop() > service){
-  			mn.addClass(mns);
+  		if($(this).scrollTop() > hdr){
+  			mn.addClass(mns_01);
   		}else{
-  			mn.removeClass(mns);
+  			mn.removeClass(mns_01);
   		}
+      if($(this).scrollTop() > service){
+        mn.addClass(mns);
+      }else{
+        mn.removeClass(mns);
+      }
   	});
 });
